1cdc6d225b031883a7569ceae9e202d4252efa0a
Tanvi Khade <tanvikhade@yahoo.in>
2022-10-30 21:09:36 +0530

    INITIAL COMMIT

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ProductList } from 'src/product-list/productList.component';
 
 import { AppComponent } from './app.component';
+import { ConvertToSpaces } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ProductList, ConvertToSpaces],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
